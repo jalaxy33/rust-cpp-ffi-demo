78fcb4601de5b46dbdd868924c9bed3a08d9a3a8
jalaxy33 <jalaxy1996@outlook.com>
2025-09-16 16:10:58 +0800

    minor update

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,3 @@ mod rust_to_cpp;
 pub use rust_to_cpp::*;
 
 pub mod call_cpp;
-
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use rust_ffi::call_cpp;
 
-
 fn main() {
     call_cpp::ffi::new_cpp_class();
     let result = call_cpp::ffi::c_add(5, 7);
